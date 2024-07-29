2581a30d8b8e79b12f117847ac8e85b4fbf72ed5
Alex Usmanov <usman0val@vk.com>
2024-07-29 11:32:57 +0300

    Commit 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: [
-    '@/assets/styles/global.scss'
-  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@use '~/assets/styles/global.scss';",
+        },
+      },
+    },
+  },
   app: {
     head: {
       link: [
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap' }
-      ]
-    }
-  }
-})
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap",
+        },
+      ],
+    },
+  },
+});
